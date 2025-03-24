184cf4204ace0f4f0a8d7e848a6cdfa26a17bf5e
Captntoaster <twanderson823@gmail.com>
2025-03-24 15:39:57 -0400

    we got a dictionary of all the characters using a set and a counter ticker. okay thanks

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ def get_book_text(path):
         return f.read()
     
 from stats import get_num_text
+from stats import get_char_count
 
 def main():
     path = "books/frankenstein.txt"
     book_text = get_book_text(path)
     word_count = get_num_text(book_text)
+    char_count = get_char_count(book_text)
     print(f"{word_count} words found in the document")
+    print(char_count)
 
 main ()
--- stats.py
+++ stats.py
@@ -1,2 +1,15 @@
 def get_num_text(book_text):
     return len(book_text.split())
+
+def get_char_count(book_text):
+    small_text = book_text.lower()
+    char_dict = {}
+    unique = set()
+    for i in small_text:
+        if i in unique:
+            char_dict[i] += 1
+        if i not in unique:
+            unique.add(i)
+            char_dict[i] = 1
+    return char_dict
+    
