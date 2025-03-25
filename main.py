4b79064fbac075878393d0aa307d91656cd616cc
Captntoaster <twanderson823@gmail.com>
2025-03-24 21:12:37 -0400

    we got the dict into a list, sorted the list, formatted the main output

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ def get_book_text(path):
     
 from stats import get_num_text
 from stats import get_char_count
+from stats import sort_chars
 
 def main():
     path = "books/frankenstein.txt"
     book_text = get_book_text(path)
     word_count = get_num_text(book_text)
     char_count = get_char_count(book_text)
-    print(f"{word_count} words found in the document")
-    print(char_count)
+    listed = sort_chars(char_count)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in listed:
+        print(f"{item['char']}: {item['count']}")
+    print("============= END ===============")
 
 main ()
--- stats.py
+++ stats.py
@@ -12,4 +12,22 @@ def get_char_count(book_text):
             unique.add(i)
             char_dict[i] = 1
     return char_dict
+
+def sort_on(listed):
+    return listed['count']
+
+def sort_chars(char_count):
+    char_list = []
+    for key, value in char_count.items():
+            if key.isalpha():
+                char_list.append({'char': key, 'count': value})
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list 
+
+
+
+
     
+
+
+   
