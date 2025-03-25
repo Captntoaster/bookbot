7d36a556b881b4a221c3f922c22443607058b6a5
Captntoaster <twanderson823@gmail.com>
2025-03-24 22:25:21 -0400

    added sys and an error message as well as making the path the sys.argv[1]

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+if len(sys.argv) == 0:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
@@ -7,7 +13,10 @@ from stats import get_char_count
 from stats import sort_chars
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
     book_text = get_book_text(path)
     word_count = get_num_text(book_text)
     char_count = get_char_count(book_text)
